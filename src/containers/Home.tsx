89881851e9b11309521036489e5000445df078f6
fandykun <fandy19061999@gmail.com>
2021-01-08 22:06:41 +0700

    landing page header / home

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, Grid, Hidden, Typography } from "@material-ui/core";
+import { Box, Grid, Hidden, Typography, Zoom, Button } from "@material-ui/core";
 import Typist from "react-typist";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+
+import Footer from "./Footer";
 
 const useStyles = makeStyles({
   root: {
@@ -10,14 +13,55 @@ const useStyles = makeStyles({
     left: "50%",
     top: "50%",
     transform: "translate(-50%, -50%)",
+    textAlign: "center",
   },
-  heavy: {
+  jobTitle: {
+    // marginTop: "-px",
+    // textDecoration: "underline",
     fontWeight: 500,
+  },
+  prim_button: {
+    margin: "20px",
+    borderRadius: "8px",
+    color: "#FFFFFF",
+    transition: "0.3s",
+    background: "#e22a25",
+    "&:hover": {
+      opacity: "1",
+      background: "#000000",
+    },
   },
 });
 
 function Home() {
   const classes = useStyles();
+
+  const [isChanged, setIsChanged] = useState(true);
+  const [indexNumber, setIndexNumber] = useState(0);
+
+  const listJobTitle = [
+    "Software Engineer",
+    "Backend Engineer",
+    "Frontend Engineer",
+  ];
+
+  const myTypist = (
+    <Typist
+      avgTypingDelay={90}
+      startDelay={100}
+      onTypingDone={() => {
+        setIsChanged(false);
+        setIndexNumber(indexNumber + 1);
+      }}
+    >
+      {listJobTitle[indexNumber % 3]}
+      <Typist.Backspace count={17} delay={200} />
+    </Typist>
+  );
+
+  useEffect(() => {
+    setIsChanged(true);
+  }, [isChanged]);
 
   return (
     <div>
@@ -25,29 +69,45 @@ function Home() {
         <Grid item xs={12}>
           <Grid
             container
-            direction="column"
+            // direction="column"
             justify="center"
-            alignItems="center"
+            // alignItems="center"
           >
-            <Hidden mdUp>
-              <Typography variant="h3" component="h4" className={classes.heavy}>
-                <Box fontWeight="fontWeightBold">Fandykun</Box>
-              </Typography>
-              <Typography variant="h6" component="h6" className={classes.heavy}>
-                <Typist avgTypingDelay={120}>Software Engineer</Typist>
-              </Typography>
-            </Hidden>
-            <Hidden smDown>
-              <Typography variant="h1" component="h2" className={classes.heavy}>
-                <Box fontWeight="fontWeightBold">Fandy Kuncoro</Box>
-              </Typography>
-              <Typography variant="h6" component="h6" className={classes.heavy}>
-                <Typist avgTypingDelay={120}>Software Engineer</Typist>
-              </Typography>
-            </Hidden>
+            <Zoom in={true} style={{ transitionDelay: "200ms" }}>
+              <Grid>
+                <Hidden mdUp>
+                  <Typography variant="h3" component="h4">
+                    <Box fontWeight="fontWeightBold">Fandykun</Box>
+                  </Typography>
+                </Hidden>
+                <Hidden smDown>
+                  <Typography variant="h1" component="h2">
+                    <Box fontWeight="fontWeightBold">Fandy Kuncoro</Box>
+                  </Typography>
+                </Hidden>
+                <Typography
+                  variant="h6"
+                  component="h6"
+                  className={classes.jobTitle}
+                >
+                  {isChanged ? myTypist : null}
+                </Typography>
+              </Grid>
+            </Zoom>
           </Grid>
+          <Button
+            variant="contained"
+            size="large"
+            disableElevation
+            className={classes.prim_button}
+            startIcon={<ExpandMoreIcon />}
+            href="#about"
+          >
+            Explore Me!
+          </Button>
         </Grid>
       </Grid>
+      <Footer />
     </div>
   );
 }
