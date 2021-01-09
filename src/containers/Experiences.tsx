e5d5ffc5fef29e09073f8e8488d5f8fc07dd47a1
fandykun <fandy19061999@gmail.com>
2021-01-09 16:53:50 +0700

    fix for deployment

--- src/containers/Experiences.tsx
+++ src/containers/Experiences.tsx
@@ -8,7 +8,7 @@ import TimelineContent from "@material-ui/lab/TimelineContent";
 import TimelineOppositeContent from "@material-ui/lab/TimelineOppositeContent";
 import TimelineDot from "@material-ui/lab/TimelineDot";
 import List from "@material-ui/core/List";
-import ListItem, { ListItemProps } from "@material-ui/core/ListItem";
+import ListItem from "@material-ui/core/ListItem";
 import ListItemIcon from "@material-ui/core/ListItemIcon";
 import ListItemText from "@material-ui/core/ListItemText";
 import Paper from "@material-ui/core/Paper";
